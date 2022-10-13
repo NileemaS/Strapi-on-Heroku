64da31246649e06a0239f529fad1b9617cd09b9a
unknown <nileemashah@yahoo.com>
2022-10-12 23:14:47 -0400

    added

---
+++ config/server.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  
+  app: {
+    keys: [ 'hoZg7SWPAzROtlm4h0LQaA==',
+            '6xaJFUt3M8vLUrUjSd49Pg==',
+            'tXIcX65oY7jhnazXuodGkg==',
+            'fqH/QhhYnkACpWnhnlBsuA==' ]
+  },
+  
+});
